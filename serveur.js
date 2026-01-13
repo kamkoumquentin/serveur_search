77abf2f9551d69f1ace190858fbc27fc731c956a
kamkoumquentin <kamkoumquentin560@gmail.com>
2026-01-13 09:32:31 +0100

    Add error handling for Cloudinary configuration
    Wrap Cloudinary configuration in a try-catch block to handle errors.

--- serveur.js
+++ serveur.js
@@ -153,11 +153,15 @@ const multer=require('multer');
  
 
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
+try {
+    cloudinary.config({
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    });
+} catch (err) {
+    console.error("ERREUR CONFIG CLOUDINARY:", err);
+}
 
 // Configuration du stockage permanent
 const storage = new CloudinaryStorage({
